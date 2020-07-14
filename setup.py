d619e9700a2061ebeff9bae83953b687d7c06990
higumachan <harekumo792154@gmail.com>
2020-07-14 19:11:07 +0900

    fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url="https://github.com/higumachan/tblog-cleaner",
     description="This is tensorboard log cleaner",
     install_requires=["tbparser", "click"],
-    dependency_links=['https://github.com/velikodniy/tbparser.git#egg=tbparser'],
+    dependency_links=['git+https://github.com/velikodniy/tbparser.git#egg=tbparser'],
     packages=["tblog_cleaner"],
     entry_points={
         "console_scripts": [
