662cc2b0db31e9b50137bb84f3f8772f7f913130
higumachan <harekumo792154@gmail.com>
2020-07-14 18:58:55 +0900

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tblog-cleaner",
-    versino="0.1.0",
+    version="0.1.0",
     url="https://github.com/higumachan/tblog-cleaner",
     description="This is tensorboard log cleaner",
     install_requires=["tblog_cleaner", "click"],
