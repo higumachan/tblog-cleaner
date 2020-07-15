2b681933b4d9682cfb12e7f963ba4c78e5107eac
higumachan <harekumo792154@gmail.com>
2020-07-15 12:50:44 +0900

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tblog-cleaner",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/higumachan/tblog-cleaner",
     description="This is tensorboard log cleaner",
     install_requires=["tbparser @ git+https://github.com/velikodniy/tbparser.git", "click"],
--- tblog_cleaner/cli.py
+++ tblog_cleaner/cli.py
@@ -6,6 +6,31 @@ from tbparser.summary_reader import SummaryReader
 import argparse
 
 
+def list_log_dir(logs_dir: Path):
+    return [log_dir for log_dir in logs_dir.glob("*") if log_dir.is_dir()]
+
+
+def log_entries(log_dir: Path, tag: str):
+    reader = SummaryReader(str(log_dir), tag_filter=[tag])
+    entries = [it for it in reader]
+    return entries
+
+
+def is_all_logs_empty(logs_dir: Path, tag: str):
+    return all([log_entries(log_dir, tag) == 0 for log_dir in list_log_dir(logs_dir)])
+
+
+def check_and_remove_logs(logs_dir: Path, threshold: int, tag: str, force: bool):
+    for log_dir in logs_dir.glob("*"):
+        if not log_dir.is_dir():
+            continue
+        reader = SummaryReader(str(log_dir), tag_filter=[tag])
+        entries = [it for it in reader]
+        if len(entries) < threshold:
+            if force or click.confirm(f"Do you want to remove {log_dir}({len(entries)} entries)"):
+                print(f"remove {log_dir}")
+                shutil.rmtree(log_dir)
+
 def main():
     parser = argparse.ArgumentParser("tblog-cleaner", description="tblog-cleaner is removing small tensorboard logs.")
     parser.add_argument("--logsdir", default="logs", help="Directory of tensorboard log dirs")
@@ -15,15 +40,11 @@ def main():
     args = parser.parse_args()
 
     logs_dir = Path(args.logsdir)
-    for log_dir in logs_dir.glob("*"):
-        if not log_dir.is_dir():
-            continue
-        reader = SummaryReader(str(log_dir), tag_filter=[args.tag])
-        entries = [it for it in reader]
-        if len(entries) < args.threshold:
-            if args.force or click.confirm(f"Do you want to remove {log_dir}({len(entries)} entries)"):
-                print(f"remove {log_dir}")
-                shutil.rmtree(log_dir)
+
+    if is_all_logs_empty(logs_dir, args.tag):
+        print(f"all logs are empty. please check tag setting \"{args.tag}\"")
+
+    check_and_remove_logs(logs_dir, args.threshold, args.tag, args.force)
 
 
 if __name__ == '__main__':
